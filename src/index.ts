431fcf2da9621fb2467888776373eb5765f2ebe4
lu55by <lussbyandros@gmail.com>
2025-11-29 23:49:20 +0800

    [LrnCommit] -> Reading and Writing Files Asynchronously.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import fs from 'fs';
 
-console.log('fs -> ', fs);
+// Read file sync using fs
+const fileInput = fs.readFileSync('txt/input.txt', 'utf8');
+console.log('fileInput -> ', fileInput);
 
-console.log("Test12");
+const outputContent = `This is what we know about the avocado: ${fileInput}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('txt/output.txt', outputContent);
