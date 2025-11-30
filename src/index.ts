aedba9a00f66fb3d30f5864962aedc63e710efc1
lu55by <lussbyandros@gmail.com>
2025-11-30 11:18:18 +0800

    [LrnCommit] -> Building a [Very] Simple API.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import fs from 'fs';
 import * as http from "node:http";
+import type {Product} from "./types/index.js";
+import {fileURLToPath} from 'url';
+import * as path from "node:path";
+
+const PORT = 8000;
+const __filename = fileURLToPath(import.meta.url);
+// Current dir of the file
+const __dirname = path.dirname(__filename);
+// Root dir
+const rootDir = path.join(__dirname, '..');
 
 ///////////////////////////////////////////
 // FILE
@@ -33,6 +43,11 @@ import * as http from "node:http";
 /////////////////////////////////////////
 // SERVER
 
+// Read the product data with fs.readFileSync at the top level
+const productDataStr: string = fs.readFileSync(`${rootDir}/dev-data/data.json`, 'utf-8');
+const productsData: Product[] = JSON.parse(productDataStr);
+console.log('Read and parsed products data -> ', productsData);
+
 // Create server using http
 const server = http.createServer((req, res) => {
     console.log('Request received.');
@@ -42,6 +57,9 @@ const server = http.createServer((req, res) => {
         res.end('Overview page');
     } else if (pathName === '/product') {
         res.end('Product page');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(productDataStr);
     } else {
         res.writeHead(404, {'Content-Type': 'text/html', 'My-Custom-Header': 'value'});
         res.end('<h1>Page Not Found!</h1>');
@@ -50,7 +68,7 @@ const server = http.createServer((req, res) => {
 });
 
 // Listen on port 8000
-server.listen(8000, 'localhost', () => {
-// server.listen(8000, '127.0.0.1', () => {
-    console.log('Server listening on port http://localhost:8000');
+server.listen(PORT, 'localhost', () => {
+// server.listen(PORT, '127.0.0.1', () => {
+    console.log(`Server listening on port http://localhost:${PORT}`);
 });
